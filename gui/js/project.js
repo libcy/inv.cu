48953013ca64dc69b578956d8e544c3133b8d426
widget <widget@pop-os.localdomain>
2018-05-19 15:35:47 +0800

    fixed linux path problems

--- gui/js/project.js
+++ gui/js/project.js
@@ -1,16 +1,30 @@
-const cwd = __dirname + '/../../';
+const path = require('path');
 const fs = require('fs');
+const cwd = path.dirname(path.dirname(__dirname));
 const config = {};
-const getdirs = (path, callback) => {
-	fs.readdir(path, (err, files) => {
+const getdirs = (pathname, callback) => {
+	fs.readdir(pathname, (err, files) => {
 		if (!err) {
 			const dirs = [];
 			for (let file of files) {
-				if (fs.statSync(path + file).isDirectory) {
+				if (fs.statSync(path.join(pathname, file)).isDirectory) {
 					dirs.push(file);
 				}
 			}
 			callback(dirs);
+		}
+		else {
+			callback([]);
+		}
+	});
+};
+const readFile = (pathname, callback) => {
+	fs.readFile(pathname, 'utf8', (err, data) => {
+		if (!err) {
+			callback(data);
+		}
+		else {
+			callback('');
 		}
 	});
 };
@@ -34,9 +48,45 @@ const navProject = document.querySelector('#nav-project');
 const navRun = document.querySelector('#nav-run');
 const mainConfig = document.querySelector('#main-config');
 const loadProject = global.loadProject = name => {
+	localStorage.setItem('inv.cu_project', name);
 	navProject.lastChild.lastChild.innerHTML = name;
-	const project_dir = cwd + 'projects/' + name + '/';
-	fs.readFile(project_dir + 'config.ini', 'utf8', (err, data) => {
+	const project_dir = path.join(cwd, 'projects', name);
+	const createEntries = () => {
+		const menuLayer = document.querySelector('#menu-layer');
+		const clickConfig = {
+			list(e) {
+
+			},
+			int(e) {
+
+			},
+			float(e) {
+
+			},
+			bool() {
+				this.cfg.value = !this.cfg.value;
+				updateConfig(this.cfg);
+			}
+		};
+		for (let cfg of config.entries) {
+			if (cfg.type === 'seperator') {
+				cfg.node = create('.seperator', mainConfig);
+				create('', cfg.name, cfg.node);
+			}
+			else if (cfg.id) {
+				cfg.node = create(mainConfig, clickConfig[cfg.type]);
+				cfg.node.cfg = cfg;
+				const text = create(cfg.node);
+				create(text);
+				create('', cfg.name, text);
+				updateConfig(cfg);
+			}
+		}
+		if (config.mode) {
+			navRun.lastChild.lastChild.innerHTML = config.mode.options[config.mode.value];
+		}
+	};
+	readFile(path.join(project_dir, 'config.ini'), data => {
 		const lines = data.split('\n');
 		for (let line of lines) {
 			if (line.indexOf('#') != -1) {
@@ -54,46 +104,11 @@ const loadProject = global.loadProject = name => {
 				}
 			}
 		}
-
-		const menuLayer = document.querySelector('#menu-layer');
-		const clickConfig = {
-			list(e) {
-
-			},
-			int(e) {
-				
-			},
-			float(e) {
-
-			},
-			bool() {
-				this.cfg.value = !this.cfg.value;
-				updateConfig(this.cfg);
-			}
-		};
-
-		for (let cfg of config.entries) {
-			if (cfg.type === 'seperator') {
-				cfg.node = create('.seperator', mainConfig);
-				create('', cfg.name, cfg.node);
-			}
-			else if (cfg.id) {
-				cfg.node = create(mainConfig, clickConfig[cfg.type]);
-				cfg.node.cfg = cfg;
-				const text = create(cfg.node);
-				create(text);
-				create('', cfg.name, text);
-				updateConfig(cfg);
-			}
-		}
-
-		if (config.mode) {
-			navRun.lastChild.lastChild.innerHTML = config.mode.options[config.mode.value];
-		}
+		createEntries();
 	});
 };
 
-fs.readFile(cwd + 'gui/config.json', 'utf8', (err, data) => {
+readFile(path.join(cwd, 'gui/config.json'), data => {
 	data = JSON.parse(data);
 	config.entries = data;
 	for (let entry of data) {
@@ -101,12 +116,13 @@ fs.readFile(cwd + 'gui/config.json', 'utf8', (err, data) => {
 			config[entry.id] = entry;
 		}
 	}
-	if (localStorage.getItem('inv.cu_project')) {
-		loadProject(localStorage.getItem('inv.cu_project'));
-	}
-	else {
-		getdirs(cwd + 'projects/', dirs => {
+	getdirs(path.join(cwd, 'projects'), dirs => {
+		const current = localStorage.getItem('inv.cu_project');
+		if (current && dirs.includes(current)) {
+			loadProject(current);
+		}
+		else {
 			loadProject(dirs[0]);
-		});
-	}
+		}
+	});
 });
--- gui/main.js
+++ gui/main.js
@@ -1,4 +1,6 @@
 const {app, Menu, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
 
 let win;
 
@@ -11,7 +13,12 @@ function createWindow() {
 		frame: process.platform != 'win32',
 		darkTheme: true
 	});
-	win.loadURL(`${__dirname}/index.html`);
+	win.loadURL(url.format({
+		pathname: path.join(__dirname, 'index.html'),
+		protocol: 'file:',
+		slashes: true
+	}));
+	win.webContents.openDevTools();
 	win.on('closed', () => {
 		win = null;
 	});
